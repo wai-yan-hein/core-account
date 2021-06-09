7c5bb853029d33cb35b5dafea40d45a24674844b
WaiYanHain <waiyanhain215@gmail.com>
2021-06-09 21:57:35 +0630

    first-commit

---
+++ src/main/java/com/inventory/CvInventoryApplication.java
@@ -0,0 +1,13 @@
+package com.inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvInventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CvInventoryApplication.class, args);
+	}
+
+}
