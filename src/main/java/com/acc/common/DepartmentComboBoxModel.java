986729f245f24dc2eec146f0e790d287298ba1bf
WaiYanHain <waiyanhain215@gmail.com>
2023-06-23 22:33:44 +0630

    w

---
+++ src/main/java/com/acc/common/DepartmentComboBoxModel.java
@@ -0,0 +1,67 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.acc.common;
+
+import com.acc.model.ChartOfAccount;
+import java.util.List;
+import javax.swing.AbstractListModel;
+import javax.swing.ComboBoxModel;
+import javax.swing.event.ListDataListener;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class COAComboBoxModel extends AbstractListModel<ChartOfAccount> implements ComboBoxModel<ChartOfAccount> {
+
+    private List<ChartOfAccount> data;
+    private ChartOfAccount selected;
+
+    public List<ChartOfAccount> getData() {
+        return data;
+    }
+
+    public void setData(List<ChartOfAccount> data) {
+        this.data = data;
+    }
+
+    @Override
+    public void setSelectedItem(Object anItem) {
+        if (anItem == null) {
+            this.selected = null;
+            fireContentsChanged(this, -1, -1);
+        } else if (anItem instanceof ChartOfAccount coa) {
+            this.selected = coa;
+            fireContentsChanged(this, -1, -1);
+        }
+
+    }
+
+    @Override
+    public Object getSelectedItem() {
+        return selected;
+    }
+
+    @Override
+    public int getSize() {
+        return data.size();
+    }
+
+    @Override
+    public ChartOfAccount getElementAt(int index) {
+        return data.get(index);
+    }
+
+    @Override
+    public void addListDataListener(ListDataListener l) {
+
+        // Not implemented
+    }
+
+    @Override
+    public void removeListDataListener(ListDataListener l) {
+        // Not implemented
+    }
+}
