82fb7084c0dccc93efc1d0278346662c45fbcad9
Ayethu-Sint <ayethusint@gmail.com>
2023-08-05 13:42:15 +0630

    opening weight/w_unit

--- src/main/java/com/inventory/ui/setup/OpeningSetup.java
+++ src/main/java/com/inventory/ui/setup/OpeningSetup.java
@@ -198,7 +198,7 @@ public class OpeningSetup extends javax.swing.JPanel implements PanelControl, Se
     private void saveOpening() {
         if (isValidEntry() && openingTableModel.isValidEntry()) {
             progress.setIndeterminate(true);
-            observer.selected("save", false);
+//            observer.selected("save", false);
             if (lblStatus.getText().equals("NEW")) {
                 OPHisKey key = new OPHisKey();
                 key.setCompCode(Global.compCode);
