4cdc24f9cfd36e6b09603aa389cc2b277e98ed19
WaiYanHain <waiyanhain215@gmail.com>
2023-10-16 10:43:45 +0630

    w

--- src/main/java/com/inventory/ui/setup/dialog/JobSetupDialog.java
+++ src/main/java/com/inventory/ui/setup/dialog/JobSetupDialog.java
@@ -411,7 +411,7 @@ public class JobSetupDialog extends javax.swing.JDialog implements KeyListener {
                     .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addComponent(txtStartDate, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(0, 9, Short.MAX_VALUE)))
+                        .addGap(0, 0, Short.MAX_VALUE)))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(jLabel5, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
